32e1cbc3031533008bf0cfc1c3f7ead0055f651e
baldimitris <52114107+baldimitris@users.noreply.github.com>
2019-11-25 14:58:00 +0200

    Create InstrumentIDMRPA.py

---
+++ SourceCode/ModulesSourceCode/InstrumentIDMRPA/InstrumentIDMRPA.py
@@ -0,0 +1,28 @@
+import SourceCode.DaedalusGlobals as DaedalusGlobals
+from datetime import datetime
+from netCDF4 import Dataset
+from shutil import copyfile
+import os
+
+"""
+Please write here a description of the module's inputs, outputs and function
+"""
+def InstrumentIDMRPA( InputFilename ):
+    # copy input file to your dedicated folder (if not exists already) in order to process it
+    simple_InputFilename = InputFilename[ InputFilename.rfind("/")+1 : InputFilename.rfind("_") ]
+    #print( simple_InputFilename )
+    #return
+    ResultFilename = DaedalusGlobals.SyntheticScience_Files_Path + simple_InputFilename + "_SyntheticScience" + ".nc"
+    if os.path.isfile(ResultFilename) == False :
+        copyfile(InputFilename, ResultFilename)
+
+    # open the file and store your calculations
+    resultCDF=Dataset(ResultFilename, "a")
+    '''
+    Here you can write source code for storing calculations into the result NetCDF file
+    '''
+    resultCDF.EditTime   = str(datetime.now())
+    resultCDF.close()    
+        
+    ##
+    return ResultFilename
