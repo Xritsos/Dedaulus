43fddfd5210ab4d15fe0acf2b103c9d8c3831fa5
baldimitris <52114107+baldimitris@users.noreply.github.com>
2019-11-26 23:29:33 +0200

    Update SGV_Ni.py

--- SourceCode/ModulesSourceCode/SGV_Ni/SGV_Ni.py
+++ SourceCode/ModulesSourceCode/SGV_Ni/SGV_Ni.py
@@ -1,7 +1,25 @@
-"""
-Write here a description of the module's inputs, outputs and function
-"""
-def SGV_Ni( InputFilename ):
-	return 0
+import SourceCode.DaedalusGlobals as DaedalusGlobals
+from datetime import datetime
+from netCDF4 import Dataset
+from shutil import copyfile
+import os
 
-
+"""
+Please write here a description of the module's inputs, outputs and function
+"""
+def SGV_Ni( InputFilename, Enabled ):
+    # copy input file to your dedicated folder (if not exists already) in order to process it
+    simple_InputFilename = InputFilename[ InputFilename.rfind("/")+1 : InputFilename.rfind("_") ]
+    ResultFilename = DaedalusGlobals.SyntheticTruth_Files_Path + simple_InputFilename + "_SyntheticTruth" + ".nc"
+    if os.path.isfile(ResultFilename) == False :
+        copyfile(InputFilename, ResultFilename)
+    if Enabled:
+        # open the file and store your calculations
+        resultCDF=Dataset(ResultFilename, "a")
+        '''
+        Here you can write source code for storing calculations into the result NetCDF file
+        '''
+        resultCDF.EditTime   = str(datetime.now())
+        resultCDF.close()    
+    ##
+    return ResultFilename
