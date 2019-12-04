3b49d624bc7674835e4328d80ba933217375ff04
baldimitris <52114107+baldimitris@users.noreply.github.com>
2019-12-04 07:13:40 +0200

    Update Test1.py
    test november plots

--- SourceCode/ModulesSourceCode/Test1/Test1.py
+++ SourceCode/ModulesSourceCode/Test1/Test1.py
@@ -4,23 +4,22 @@ import numpy as np
 
 def Test1():
     print( "Creating..." )
-    #s = PlotGlobe.PlotGlobe( "DAED_ORB_Evt0_LLA_Per120_Lat00_Srt01Hz_Msc.csv", "../../DataFiles/orbit_IRIresult.csv", "ne iri16", "cm^-3", "Jet" )
-    #s = PlotGlobe.PlotGlobe( "", "../../DataFiles/orbit_IRIresult.csv", "Electron Density currents", "cm^-3", "Jet" )
-    #s = PlotGlobe.PlotGlobe( "../../DataFiles/sphere_IRIresult.csv", "", "Electron Density currents", "cm^-3", "Jet" )
-    #s = PlotGlobe.PlotGlobe( "sphere_IRI16_cities.csv", "", "Electron Density currents", "cm^-3", "Jet" )
 
-    #s = PlotGlobe.PlotGlobe( "Surface.nc", "ALFA", "cm^-3", "Jet",    "Orbit.nc", "ALFA", "cm^-3", "Jet",    "Electron Density currents" )
-    #s = PlotGlobe.PlotGlobe( "sphere_IRI16_cities.csv", "Te_iri16_K", "cm^-3", "Jet",   "Orbit.nc", "Oxygen", "cm^-3", "Jet",   "Electron Density currents" )
+    # Plot Magnetic Field Unit Vectors 
+    s = PlotGlobe.PlotGlobe( "", "O2", "nT", "Jet",    "", "ALFA", "cm^-3", "Jet",    "Magnetic Field (B unit)", "/home/NAS/TIEGCM_DATA/TIEGCM_EF/E_field_TIEGCMnew.nc", "B_Unit_x,B_Unit_y,B_Unit_z", "nT", None )
+    
+    # Plot Magnetic Field Vectors 
+    s = PlotGlobe.PlotGlobe( "", "O2", "nT", "Jet",    "", "ALFA", "cm^-3", "Jet",    "Magnetic Field (B)", "/home/NAS/TIEGCM_DATA/TIEGCM_EF/E_field_TIEGCMnew.nc", "Bx,By,Bz", "nT", "Jet" )
+    
+    # Plot Electric Field Vectors
+    #s = PlotGlobe.PlotGlobe( "", "Ephi", "V/m", "Jet",    "", "ALFA", "cm^-3", "Jet",    "Electic Field (data are Log10-scaled)", "/home/NAS/TIEGCM_DATA/TIEGCM_EF/E_field_TIEGCMnew.nc", "Ex,Ey,Ez", "V/m", "Jet",SurfaceOpacity=1  ) #s = PlotGlobe.PlotGlobe( "/home/NAS/TIEGCM_DATA/TIEGCM_EF/E_field_TIEGCMnew.nc", "Ephi", "V/m", "Jet",    "", "ALFA", "cm^-3", "Jet",    "Electic Field (data are Log10-scaled)", "/home/NAS/TIEGCM_DATA/TIEGCM_EF/E_field_TIEGCMnew.nc", "Ex,Ey,Ez", "V/m", "gray",SurfaceOpacity=1, LogScale=True  )
+    
+    # Plot Neutral Winds Vectors
+    #s = PlotGlobe.PlotGlobe( "/home/NAS/TIEGCM_DATA/TIEGCM_EVT1_2015_StPatricksDay_HAO/tiegcm_dres.s_mar2015_amie_v1_01.nc", "UN", "cm/s", "Jet",    "", "ALFA", "cm^-3", "Jet",    "Neutral Winds", SurfaceOpacity=1 )
+    
+    # Plot Joule Heating
+    s = PlotGlobe.PlotGlobe( "/home/NAS/TIEGCM_DATA/TIEGCM_EF/tiegcm_dres.s_mar2015_amie_v1_01_JH.nc", "Joule_Heating", "W/m^3", "Jet",    "/home/NAS/TIEGCM_DATA/TIEGCM_EF/Daedalus_Interpolator_JH.nc", "Joule_Heating", "W/m^3", "Jet",    "Joule Heating (data are Log10-scaled)", SurfaceOpacity=1, LogScale=True )
 
-    #s = PlotGlobe.PlotGlobe( "/home/NAS/Data_Files/Temp/Surface.nc", "ALFA", "NO", "Jet",    "", "ALFA", "cm^-3", "Jet",    "O2" )
-
-    s = PlotGlobe.PlotGlobe( "", "O2", "nT", "Jet",    "", "ALFA", "cm^-3", "Jet",    "Magnetic Field (nT)", "/home/NAS/TIEGCM_DATA/TIEGCM_EF/E_field_TIEGCMnew.nc", "B", "nT", "Jet" )
-
-    s = PlotGlobe.PlotGlobe( "/home/NAS/TIEGCM_DATA/TIEGCM_EF/E_field_TIEGCMnew.nc", "Ephi", "V/m", "Jet",    "", "ALFA", "cm^-3", "Jet",    "Electic Field", "/home/NAS/TIEGCM_DATA/TIEGCM_EF/E_field_TIEGCMnew.nc", "E", "E", "gray",SurfaceOpacity=1  )
-
-    s = PlotGlobe.PlotGlobe( "/home/NAS/TIEGCM_DATA/TIEGCM_EVT1_2015_StPatricksDay_HAO/tiegcm_dres.s_mar2015_amie_v1_01.nc", "UN", "cm/s", "Jet",    "", "ALFA", "cm^-3", "Jet",    "Neutral Winds", SurfaceOpacity=1 )
-
-    s = PlotGlobe.PlotGlobe( "/home/NAS/TIEGCM_DATA/TIEGCM_EF/tiegcm_dres.s_mar2015_amie_v1_01_JH.nc", "Joule_Heating", "W/m^3", "Jet",    "/home/NAS/TIEGCM_DATA/TIEGCM_EF/Daedalus_Interpolator_JH.nc", "Joule_Heating", "W/m^3", "Jet",    "Joule Heating", SurfaceOpacity=1 )
-
+    
     print(s)
     return s
